68417ba7283ac5c065414995a7b6d8b95b087a88
Ahmed Abdullah <Ahmed_A.Qadir@users.github.com>
2019-07-11 02:29:28 +0300

    using JSON to insert and show data of mongoDB

--- api/index.js
+++ api/index.js
@@ -1,33 +1,59 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var path = require('path');
-var app = express();
-require('./config/server');
 
-//to get data from html and send into database
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
+var mongoose = require('mongoose');
+var express = require('express');
+var app = express();
+app.use(express.json());
+var body = app.listen(process.env.PORT || 3000);
+
+//connecting to database and creating database if database doesnt exist
+mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true })
+    .then(() => console.log('connected succsessfully'))
+    .catch(() => console.log('could not connect to database'));
+
+
+//make a structure to ur database
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean,
+});
+
+
+//make a class for this scheme 
+const CourseClass = mongoose.model('Course', courseSchema);
 
 
 //inserting data
-app.post('/post-feedback', function (req, res) {
-    var conn = mongoose.connection;
-    var user = {
-        _id:req.body.ids,
-        name: req.body.personname,
-    };
-    
-    conn.collection('customers').insertOne(user);
-    console.log("Data Inserted");
-    res.redirect('/');
+app.post('/insert', (req, res) => {
+    createCourse(req);
 });
 
-//opening and directing main page to anypage u want
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+//showing data
+app.get('/show', (req, res) => {
+    getCourses(res);
 });
 
 
 
-const port = process.env.PORT || 3000;
-app.listen(port);
+
+
+//createing an object fr our class it means making and instance for my class
+async function createCourse(req) {
+    const course = new CourseClass({
+        name: req.body.name,
+        author: "Ahmed",
+        tags: ['node', 'backend'],
+        isPublished: true,
+    })
+    const result = await course.save();
+    console.log("data has been inserted ");
+}
+
+//using query to find this data that we want
+async function getCourses(res) {
+    const result = await CourseClass.find({});
+    res.json(result);
+    console.log("Data showen");
+}
